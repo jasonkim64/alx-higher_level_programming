b75f99268aa7e1c608d792de24e44ed712833ad0
samuel <jk1746823@gmail.com>
2024-03-14 02:19:36 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{} argument.".format(x))
+    elif x == 1:
+        print("{} argument.".format(x))
+    else:
+        print("{} argument.".format(x))
+
+    for i in range(x):
+        print("{}: {:s}".format(i + i,argv[i + 1]))
